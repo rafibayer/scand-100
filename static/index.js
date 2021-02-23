dba1a1c3eecda07ce50072142767d78d4517572d
Rafael Bayer <rafibayer7@gmail.com>
2021-02-22 16:23:22 -0800

    dropdown javascript works but css very ugly :(

---
+++ static/index.js
@@ -0,0 +1,21 @@
+'use strict';
+(() => {
+
+    window.onload = () => {
+        let countryHeaders = document.getElementsByClassName("countries")[0].childNodes;
+        countryHeaders.forEach(header => {
+            header.addEventListener("click", () => {
+                showCountryArticle(header.innerText);
+            })
+        });
+    }
+
+    function showCountryArticle(countryName) {
+        let allArticles = Array.from(document.getElementsByClassName("country"));
+        allArticles.forEach(article => article.classList.add("hidden"));
+
+        let articleToShowID = countryName+"Article";
+        document.getElementById(articleToShowID).classList.remove("hidden");
+    }
+
+})();
